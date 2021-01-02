954aafeeb2d8002ad37d8a0ef2e075274cf77ede
adiguno <25011351+adiguno@users.noreply.github.com>
2021-01-01 21:13:26 -0500

    added new text-gen comp to app.comp

---
+++ articulated-nonsense/src/app/text-gen/text-gen.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-text-gen',
+  templateUrl: './text-gen.component.html',
+  styleUrls: ['./text-gen.component.css']
+})
+export class TextGenComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
